4ed607f2e83f03f1776812ba2e4ebb34512049ad
nekomaneki <mjuliagirona@gmail.com>
2017-07-07 00:00:48 -0300

    clase Words

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -1,10 +1,13 @@
 package com.example.android.miwok;
 
+import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 /**
  * Created by julia on 7/6/2017.
@@ -15,5 +18,9 @@ public class WordAdapter extends ArrayAdapter<Word> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         return super.getView(position, convertView, parent);
+    }
+
+    public WordAdapter(Activity context, ArrayList<Word> words) {
+        super(context, 0, words);
     }
 }
