ef69558d8e43d632eeb4c099b416c6964464593a
nekomaneki <mjuliagirona@gmail.com>
2017-07-06 23:37:24 -0300

    shit just got real

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +13,70 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+//        words.add("one");
+
+//        Word w = new Word("one", "ichi");
+//        words.add(w);
+
+
+        words.add(new Word ("one", "ichi"));
+        words.add(new Word ("one", "ni"));
+        words.add(new Word ("one", "san"));
+        words.add(new Word ("one", "yon"));
+        words.add(new Word ("one", "go"));
+        words.add(new Word ("one", "roku"));
+        words.add(new Word ("one", "nana"));
+        words.add(new Word ("one", "hachi"));
+        words.add(new Word ("one", "kyuu"));
+        words.add(new Word ("one", "juu"));
+
+
+//        int index = 0;
+//
+//        while (index<words.size()) {
+//            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//            TextView wordView = new TextView(this);
+//            wordView.setText(words.get(index));
+//            rootView.addView(wordView);
+//
+//            index++;
+//        }
+
+        //Find the root view of the whole layout
+//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
+//
+//        for (int index = 0; index <words.size(); index++) {
+//
+//            //Create a new TextView
+//            TextView wordView = new TextView(this);
+//
+//            //Set the text to be word at the current index
+//            wordView.setText(words.get(index));
+//            //Add this TextView as another child to the root view of this layout
+//            rootView.addView(wordView);
+//        }
+
+//        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
+//
+//        ListView listView = (ListView) findViewById(R.id.list);
+//
+//        listView.setAdapter(itemsAdapter);
+
+        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
+        // adapter knows how to create list items for each item in the list.
+        WordAdapter adapter = new WordAdapter(this, words);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // activity_numbers.xml layout file.
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
+        // {@link ListView} will display list items for each {@link Word} in the list.
+        listView.setAdapter(adapter);
+
     }
 }
---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,9 @@
+package com.example.android.miwok;
+
+/**
+ * Created by julia on 7/6/2017.
+ */
+
+public class Word {
+
+}
