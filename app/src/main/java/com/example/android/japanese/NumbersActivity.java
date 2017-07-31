3bb5d77a8bf2e47ba1fb8e1cccff036a6a8047cb
nekomaneki <mjuliagirona@gmail.com>
2017-07-30 23:41:52 -0300

    agregue setonitemclicklistener para que magicamente reproduzca audios

--- app/src/main/java/com/example/android/japanese/NumbersActivity.java
+++ app/src/main/java/com/example/android/japanese/NumbersActivity.java
@@ -1,12 +1,18 @@
 package com.example.android.japanese;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,55 +20,19 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.words_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-
-//        words.add("one");
-
-//        Word w = new Word("one", "ichi");
-//        words.add(w);
+        mediaPlayer = MediaPlayer.create(this, R.raw.number_one);
 
 
-        words.add(new Word ("one", "ichi", R.drawable.number_one));
-        words.add(new Word ("two", "ni", R.drawable.number_two));
-        words.add(new Word ("three", "san", R.drawable.number_three));
-        words.add(new Word ("four", "yon", R.drawable.number_four));
-        words.add(new Word ("five", "go", R.drawable.number_five));
-        words.add(new Word ("six", "roku", R.drawable.number_six));
-        words.add(new Word ("seven", "nana", R.drawable.number_seven));
-        words.add(new Word ("eight", "hachi", R.drawable.number_eight));
-        words.add(new Word ("nine", "kyuu", R.drawable.number_nine));
-        words.add(new Word ("ten", "juu", R.drawable.number_ten));
-
-
-//        int index = 0;
-//
-//        while (index<words.size()) {
-//            LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-//            TextView wordView = new TextView(this);
-//            wordView.setText(words.get(index));
-//            rootView.addView(wordView);
-//
-//            index++;
-//        }
-
-        //Find the root view of the whole layout
-//        LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
-//
-//        for (int index = 0; index <words.size(); index++) {
-//
-//            //Create a new TextView
-//            TextView wordView = new TextView(this);
-//
-//            //Set the text to be word at the current index
-//            wordView.setText(words.get(index));
-//            //Add this TextView as another child to the root view of this layout
-//            rootView.addView(wordView);
-//        }
-
-//        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, words);
-//
-//        ListView listView = (ListView) findViewById(R.id.list);
-//
-//        listView.setAdapter(itemsAdapter);
+        words.add(new Word("one", "ichi", R.drawable.number_one));
+        words.add(new Word("two", "ni", R.drawable.number_two));
+        words.add(new Word("three", "san", R.drawable.number_three));
+        words.add(new Word("four", "yon", R.drawable.number_four));
+        words.add(new Word("five", "go", R.drawable.number_five));
+        words.add(new Word("six", "roku", R.drawable.number_six));
+        words.add(new Word("seven", "nana", R.drawable.number_seven));
+        words.add(new Word("eight", "hachi", R.drawable.number_eight));
+        words.add(new Word("nine", "kyuu", R.drawable.number_nine));
+        words.add(new Word("ten", "juu", R.drawable.number_ten));
 
         // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
         // adapter knows how to create list items for each item in the list.
@@ -71,7 +41,17 @@ public class NumbersActivity extends AppCompatActivity {
         // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
         // There should be a {@link ListView} with the view ID called list, which is declared in the
         // words_listlayout file.
-        ListView listView = (ListView) findViewById(R.id.list);
+        final ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                mediaPlayer.start();
+            }
+
+
+        });
+
 
         // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
         // {@link ListView} will display list items for each {@link Word} in the list.
--- app/src/main/java/com/example/android/japanese/WordAdapter.java
+++ app/src/main/java/com/example/android/japanese/WordAdapter.java
@@ -13,9 +13,6 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
-/**
- * Created by julia on 7/6/2017.
- */
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
